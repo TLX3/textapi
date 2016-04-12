53a8c850a1fce9f64ae8707f539b07c331d5352d
TLX3 <tdl2116@columbia.edu>
2016-04-12 11:13:57 -0400

    Complete sentences/avg_len

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,2 +1,9 @@
 class SentencesController < ApplicationController
+  def avg_len
+    text = params[:text]
+    ps = PragmaticSegmenter::Segmenter.new(text: text, clean: true)
+    lengths = ps.segment.map { |s| s.length }
+    avg = (lengths.inject(:+))/lengths.size
+    render :json => avg.to_json
+  end
 end
